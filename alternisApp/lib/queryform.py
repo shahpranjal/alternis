bed5c4c4c8f214c0e5cdada549c496840fa23b2a
Mackenzie Gray <mackenziegray4@gmail.com>
2014-09-21 00:56:10 -0400

    actually working searchbar

--- alternisApp/lib/queryform.py
+++ alternisApp/lib/queryform.py
@@ -1,9 +1,19 @@
 from django import forms
 
 class QueryForm(forms.Form):
+
+    def set_initial(self):
+        q = forms.CharField(
+            widget=forms.TextInput(attrs={'class': 'form-control'}),
+            max_length=200,
+            initial=self,
+            label=""
+        )
+        return q
+
     q = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control'}),
-        max_length=200,
-        initial='looking for some competition?',
-        label=""
+            widget=forms.TextInput(attrs={'class': 'form-control'}),
+            max_length=200,
+            initial='looking for some competition?',
+            label=""
         )
