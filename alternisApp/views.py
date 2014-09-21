cc858c15000b8a4cdeb196f2e6d80a6b45e6d5e4
Shayan Masood <zmasood.shayan@gmail.com>
2014-09-21 01:01:52 -0400

    More merge

--- alternisApp/views.py
+++ alternisApp/views.py
@@ -1,8 +1,7 @@
 import json
 from django.http import HttpResponse
-from alternisApp.lib.queryapi import google_query,bing_query,correction_query
 from django.shortcuts import render
-from alternisApp.lib.queryapi import google_query,bing_query
+from alternisApp.lib.queryapi import google_query,bing_query, correction_query
 from alternisApp.lib.queryform import QueryForm
 
 
@@ -20,16 +19,16 @@ def query(request):
     results_list = list(set().union(*[google_results,bing_results]))
     return render(request, 'alternis/results.html', { 'q': q, 'gr': results_list})
 
-def query(request, Search):
-    # t = loader.get_template('query.html')
-    # c = Context({
-    #
-    # })
-    #return HttpResponse(t.render(c))
-    firstSearchResult = ''.join(correction_query(Search))
-    setUnion = list(set().union(*[google_query(Search),bing_query(Search)]))
-    if Search.upper().replace(' ', '') == firstSearchResult.upper().replace(' ',''):
-        return HttpResponse(json.dumps(setUnion))
-    #return HttpResponse(json.dumps(setUnion))
-    return HttpResponse('Correction ' + firstSearchResult)
+# def query(request, Search):
+#     # t = loader.get_template('query.html')
+#     # c = Context({
+#     #
+#     # })
+#     #return HttpResponse(t.render(c))
+#     firstSearchResult = ''.join(correction_query(Search))
+#     setUnion = list(set().union(*[google_query(Search),bing_query(Search)]))
+#     if Search.upper().replace(' ', '') == firstSearchResult.upper().replace(' ',''):
+#         return HttpResponse(json.dumps(setUnion))
+#     #return HttpResponse(json.dumps(setUnion))
+#     return HttpResponse('Correction ' + firstSearchResult)
 
