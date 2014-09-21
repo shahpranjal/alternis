8764b4c83c3513f5088643ee0fa16a8f3f511a0a
Shayan Masood <zmasood.shayan@gmail.com>
2014-09-20 22:16:15 -0400

    readablecode

--- alternisApp/lib/queryapi.py
+++ alternisApp/lib/queryapi.py
@@ -1,11 +1,10 @@
 import urllib2
-import urllib
 import xml.etree.ElementTree as ET
 import json
 
 # Query Google API
 def google_query(Search):
-    search_str = Search + '+vs'
+    search_str = Search.replace(' ', '+') + '+vs'
     url = 'http://google.com/complete/search?output=toolbar&q=' + search_str
     serialized_data = urllib2.urlopen(url).read()
     tree = ET.fromstring(serialized_data)
@@ -13,29 +12,26 @@ def google_query(Search):
     if (tree):
         for rivals in tree:
             rawData = (rivals[0].attrib['data'])
-            if ('vs' in rawData):
-                indexOf = rawData.find('vs')
-                if (rawData[indexOf+2:] != ''):
-                    competitor.append(rawData[indexOf+2:])
+            if ' vs ' in rawData:
+                competitor = list(set().union(*[competitor, extract_vs(rawData)]))
     return competitor
 
 # Query Bing API
 def bing_query(Search):
-    search_str = Search + '+vs'
+    search_str = Search.replace(' ', '+') + '+vs'
     competitor = []
     url = 'http://api.bing.com/osjson.aspx?query=' + search_str
     jsonVal= json.loads(urllib2.urlopen(url).read())
     if (jsonVal):
         for rivals in jsonVal[1]:
-            if ('vs' in rivals):
-                indexOf = rivals.find('vs')
-                competitor.append(rivals[indexOf+2:])
+            if ' vs ' in rivals:
+                competitor = list(set().union(*[competitor, extract_vs(rivals)]))
     return competitor
 
 
 #DidYouMean?
 def correction_query(Search):
-    url = 'http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=' + Search + '&gl=us'
+    url = 'http://suggestqueries.google.com/complete/search?output=toolbar&hl=en&q=' + Search.replace(' ', '+') + '&gl=us'
     serialized_data = urllib2.urlopen(url).read()
     tree = ET.fromstring(serialized_data)
     if (tree):
@@ -52,4 +48,7 @@ def google_image_search(query):
     if not (jsonVal and "items" in jsonVal and "link" in jsonVal["items"][0]):
         return None
     return jsonVal["items"][0]["link"]
+
+def extract_vs(str):
+    return str.split(' vs ')
 
